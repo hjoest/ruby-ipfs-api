734dc484b48a564a1b1899e7e93011f998be1de5
Holger Joest <holger@joest.org>
2015-12-29 16:13:47 +0100

    Add support for debugging with byebug
    If you wish to enable byebug for the unit tests, just run
    
          DEBUG=true rake test

--- test/common.rb
+++ test/common.rb
@@ -1,5 +1,9 @@
 gem 'minitest' if RUBY_VERSION =~ /^1\.9/
 require 'minitest/autorun'
-require 'byebug'
+if ENV['DEBUG']
+  require 'byebug'
+else
+  def byebug; end
+end
 
 $:.unshift File.expand_path('../../lib', __FILE__)

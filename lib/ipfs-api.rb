cbd1c1b03914c38e4a8e4a7758a31c97e5b38c57
Holger Joest <holger@joest.org>
2017-02-05 20:15:08 +0100

    Release version 0.3, update README

--- lib/ipfs-api.rb
+++ lib/ipfs-api.rb
@@ -1,10 +1,10 @@
-# =IPFS4R - Interplanetary Filesystem Library for Ruby
+# =IPFS API for Ruby
 # License:: MIT (see the LICENSE file)
 # Website:: https://hjoest.github.io/ruby-ipfs-api
 #
 # ==Introduction
 #
-# IPFS4R is an IPFS[https://ipfs.io] library for Ruby.
+# IPFS API for Ruby is an IPFS[https://ipfs.io] library for Ruby.
 #
 require 'ipfs-api/version'
 require 'ipfs-api/io'
--- lib/ipfs-api/version.rb
+++ lib/ipfs-api/version.rb
@@ -1,4 +1,4 @@
 module IPFS
-  VERSION = '0.2.0'
+  VERSION = '0.3.0'
 end
 
